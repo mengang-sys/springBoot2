7030dd8ea71f4502962b4123e400e0e7772db38a
mengang-sys <377532225@qq.com>
2021-01-02 19:46:05 +0800

    [megnyang].exception

--- src/main/java/com/example/demo/exception/CustomerBusinessExceptionHandler.java
+++ src/main/java/com/example/demo/exception/CustomerBusinessExceptionHandler.java
@@ -1,4 +1,21 @@
 package com.example.demo.exception;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class CustomerBusinessExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(BusinessException.class)
+    public Map<String,Object> businessExceptionHandler(BusinessException e){
+        Map<String,Object> map = new HashMap<>();
+        map.put("code",e.getCode());
+        map.put("message",e.getMessage());
+        return map;
+    }
 }
--- src/main/java/com/example/demo/exception/TestController.java
+++ src/main/java/com/example/demo/exception/TestController.java
@@ -1,4 +1,17 @@
 package com.example.demo.exception;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @RequestMapping("/BusinessException")
+    public String testResponseStatusExceptionResolver(@RequestParam("i") int i){
+        if(i==0){
+            throw new BusinessException(600,"自定义业务错误");
+        }
+        return "success";
+    }
 }
